90a40795bff2ee04748b760f59b8e197133b0e0f
durimfetahaj <131853939+durimfetahaj@users.noreply.github.com>
2026-02-17 16:33:47 +0100

    made nav fixed on scroll

--- components/layout/header/desktop-header.tsx
+++ components/layout/header/desktop-header.tsx
@@ -5,34 +5,32 @@ import { cn } from "@/lib/utils";
 import { navLinks } from "@/components/layout/constants/nav-links";
 import { LearnMoreBtn } from "./learn-more-btn";
 import { Logo } from "./logo";
+import { useEffect, useState } from "react";
 
 export const DesktopHeader = () => {
-  // const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     // Triggering at 20px ensures the styles are gone
-  //     // well before the user "lands" at scroll 0.
-  //     setIsScrolled(window.scrollY > 300);
-  //   };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 80);
+    };
 
-  //   window.addEventListener("scroll", handleScroll, { passive: true });
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
+    window.addEventListener("scroll", handleScroll, { passive: false });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <header className="flex justify-between items-center py-5 pb-20 w-full px-5 relative">
       <Logo />
 
-      {/* Wrapper to preserve space in the flexbox */}
       <div className="flex-1 flex justify-center">
         <nav
           className={cn(
-            "hidden md:flex items-center ",
-            // "bg-white/5 backdrop-blur-2xl border border-white/10 rounded-full px-10 py-4",
-            // isScrolled
-            //   ? "fixed top-6 shadow-[0_8px_32px_0_rgba(0,0,0,0.03)]"
-            //   : "relative top-0 left-0",
+            "hidden md:flex items-center transition-all duration-300 ease-in-out",
+            "fixed left-1/2 -translate-x-1/2 z-50",
+            isScrolled
+              ? "top-6 scale-100 opacity-100 bg-white/5 backdrop-blur-2xl py-4 px-10 border border-white/10 rounded-full shadow-[0_8px_32px_0_rgba(0,0,0,0.03)]"
+              : "top-0 scale-95 opacity-90 bg-transparent py-6 px-0 border border-transparent shadow-none",
           )}
         >
           <div className="flex items-center gap-8">
